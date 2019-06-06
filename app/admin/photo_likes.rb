1303ecefa3c201854fc2b9a8092a454f9c6216d7
Raghu Betina <raghu@firstdraft.com>
2019-06-06 19:18:00 +0000

    Whitelist columns in PhotoLike for ActiveAdmin

--- app/admin/photo_likes.rb
+++ app/admin/photo_likes.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register PhotoLike do
+
+ permit_params :photo_id, :user_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
