a8cdc3cb5e8e2b3d5dd4ce39db71a3476682a4f2
Raghu Betina <raghu@firstdraft.com>
2019-06-06 19:17:49 +0000

    Whitelist columns in VenueComment for ActiveAdmin

--- app/admin/venue_comments.rb
+++ app/admin/venue_comments.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register VenueComment do
+
+ permit_params :author_id, :venue_id, :body
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
